8ca9c8f521fc1dc998b770b6c06ab136a16927e6
Lachlan Turner <lachlan.turner@gmail.com>
2018-05-25 17:02:43 +0930

    Added JUnit

---
+++ src/DurationInfoTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class DurationInfoTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getDaysBetweenDateTimes() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getWeekdaysBetweenDateTimes() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getCompleteWeeksBetweenDateTimes() {
+    }
+}

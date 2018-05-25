e0efb07c97699fd7449c3b5489a90403ae27be6e
Lachlan Turner <lachlan.turner@gmail.com>
2018-05-25 19:22:46 +0930

    Implemented method to return weekdays between date times. Also added helper method to set both start and end date times together.

--- src/DurationInfo.java
+++ src/DurationInfo.java
@@ -1,5 +1,7 @@
+import java.time.DayOfWeek;
 import java.time.OffsetDateTime;
 import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjusters;
 
 import static java.time.temporal.ChronoUnit.DAYS;
 import static java.time.temporal.ChronoUnit.WEEKS;
@@ -17,6 +19,11 @@ public class DurationInfo {
         this.endDateTime = endDateTime;
     }
 
+    public void setDateTimes(OffsetDateTime startDateTime, OffsetDateTime endDateTime) {
+        this.startDateTime = startDateTime;
+        this.endDateTime = endDateTime;
+    }
+
     // method used to return difference between date times in given units
     public long getUnitsBetweenDateTimes(ChronoUnit units) {
         return units.between(startDateTime, endDateTime);
@@ -26,8 +33,57 @@ public class DurationInfo {
         return getUnitsBetweenDateTimes(DAYS);
     }
 
+    /**
+     * Finds the number of weekdays (MON-FRI) between two date times.
+     * If the start day is a weekend, it will be considered to start at midnight on the following Monday.
+     * For example, if the two date times are:
+     *  - Saturday, 19th May, 2018, 10:00
+     *  - Wednesday, 23rd May, 2018, 11:00
+     *  Then the number of weekdays will be 2 (number of whole days between Monday, 12am and Wednesday, 11am).
+     *
+     * If the end day is a weekend, it will be considered to end at midnight on the previous (or same) Saturday.
+     * For example, if the two date times are:
+     *  - Tuesday, 22nd May, 2018, 10:00
+     *  - Sunday, 27rd May, 2018, 11:00
+     * Then the number of weekdays will be 3 (number of whole days between Tuesday, 10am and Saturday, 12am).
+     * @return The number of weekdays between start and end date times.
+     */
     public long getWeekdaysBetweenDateTimes() {
-        return 0;
+        DayOfWeek startDay = startDateTime.getDayOfWeek();
+        DayOfWeek endDay = endDateTime.getDayOfWeek();
+
+        OffsetDateTime originalStartDateTime = startDateTime;
+        OffsetDateTime originalEndDateTime = endDateTime;
+
+        // adjust start date time if it falls on the weekend
+        if (DayOfWeek.SATURDAY.equals(startDay) || DayOfWeek.SUNDAY.equals(startDay)) {
+            startDateTime = startDateTime.with(TemporalAdjusters.next(DayOfWeek.MONDAY)).truncatedTo(DAYS);
+            startDay = DayOfWeek.MONDAY;
+        }
+
+        // adjust end date time if it falls on the weekend
+        if (DayOfWeek.SATURDAY.equals(endDay) || DayOfWeek.SUNDAY.equals(endDay)) {
+            endDateTime = endDateTime.with(TemporalAdjusters.previousOrSame(DayOfWeek.SATURDAY)).truncatedTo(DAYS);
+            endDay = DayOfWeek.SATURDAY;
+        }
+
+        // find the number of days between the dates
+        long days = getDaysBetweenDateTimes();
+        long weeks = getCompleteWeeksBetweenDateTimes();
+
+        // remove weekends by subtracting 2 for each week
+        long weekdays = days - (2 * weeks);
+
+        // if the start date time is after the end date time (in terms of relative position in the week), then another weekend needs to be accounted for
+        if (startDay.getValue() > endDay.getValue() || (startDay.getValue() == endDay.getValue() && startDateTime.toLocalTime().isAfter(endDateTime.toLocalTime()))) {
+            weekdays = weekdays - 2;
+        }
+
+        // reset start and end date times to original values
+        startDateTime = originalStartDateTime;
+        endDateTime = originalEndDateTime;
+
+        return weekdays;
     }
 
     public long getCompleteWeeksBetweenDateTimes() {
--- src/DurationInfoTest.java
+++ src/DurationInfoTest.java
@@ -55,6 +55,43 @@ class DurationInfoTest {
 
     @org.junit.jupiter.api.Test
     void getWeekdaysBetweenDateTimes() {
+        // create date times for january 1 and february 1 at noon
+        OffsetDateTime january1st = OffsetDateTime.of(2018, 1, 1, 12, 0, 0, 0, ZoneOffset.UTC);
+        OffsetDateTime feb1st = OffsetDateTime.of(2018, 2, 1, 12, 0, 0, 0, ZoneOffset.UTC);
+        // also create date time for january 1 at 5pm
+        OffsetDateTime january1st5pm = OffsetDateTime.of(2018, 1, 1, 17, 0, 0, 0, ZoneOffset.UTC);
+        OffsetDateTime january8th = OffsetDateTime.of(2018, 1, 8, 12, 0, 0, 0, ZoneOffset.UTC);
+
+        // create date times with offsets
+        OffsetDateTime dtCompare1 = OffsetDateTime.of(2018, 1, 1, 1, 0, 0, 0, ZoneOffset.ofHours(10));
+        OffsetDateTime dtCompare2 = OffsetDateTime.of(2018, 1, 1, 20, 0, 0, 0, ZoneOffset.ofHours(1));
+
+        // create dates on weekend
+        OffsetDateTime wkndStart = OffsetDateTime.of(2018, 5, 6, 12, 0, 0, 0, ZoneOffset.UTC);
+        OffsetDateTime wkndEnd = OffsetDateTime.of(2018, 5, 26, 12, 0, 0, 0, ZoneOffset.UTC);
+
+        durationInfo.setDateTimes(january1st, january1st);
+        long weekdays = durationInfo.getWeekdaysBetweenDateTimes();
+        assertEquals(0, weekdays);
+
+        durationInfo.setDateTimes(january1st, feb1st);
+        weekdays = durationInfo.getWeekdaysBetweenDateTimes();
+        // 31 days in January, and 4 weekends, so expect 23 weekdays (31 - 8) as result
+        assertEquals(23, weekdays);
+
+        durationInfo.setDateTimes(january1st5pm, january8th);
+        weekdays = durationInfo.getWeekdaysBetweenDateTimes();
+        // slightly less than complete week - expect 4 weekdays
+        assertEquals(4, weekdays);
+
+        durationInfo.setDateTimes(dtCompare1, dtCompare2);
+        weekdays = durationInfo.getWeekdaysBetweenDateTimes();
+        // equivalent UTC times are 2017-12-31 15:00 and 2018-01-01 19:00, so should return 1
+        assertEquals(1, weekdays);
+
+        durationInfo.setDateTimes(wkndStart, wkndEnd);
+        weekdays = durationInfo.getWeekdaysBetweenDateTimes();
+        assertEquals(15, weekdays);
     }
 
     @org.junit.jupiter.api.Test
@@ -70,25 +107,21 @@ class DurationInfoTest {
         OffsetDateTime dtCompare1 = OffsetDateTime.of(2018, 1, 1, 1, 0, 0, 0, ZoneOffset.ofHours(10));
         OffsetDateTime dtCompare2 = OffsetDateTime.of(2018, 1, 7, 20, 0, 0, 0, ZoneOffset.ofHours(1));
 
-        durationInfo.setStartDateTime(january1st);
-        durationInfo.setEndDateTime(january1st);
+        durationInfo.setDateTimes(january1st, january1st);
         long weeks = durationInfo.getCompleteWeeksBetweenDateTimes();
         assertEquals(0, weeks);
 
-        durationInfo.setStartDateTime(january1st);
-        durationInfo.setEndDateTime(feb1st);
+        durationInfo.setDateTimes(january1st, feb1st);
         weeks = durationInfo.getCompleteWeeksBetweenDateTimes();
         // 31 days in January, so expect 31 days = 4 complete weeks as result
         assertEquals(4, weeks);
 
-        durationInfo.setStartDateTime(january1st5pm);
-        durationInfo.setEndDateTime(january8th);
+        durationInfo.setDateTimes(january1st5pm, january8th);
         weeks = durationInfo.getCompleteWeeksBetweenDateTimes();
         // slightly less than complete week
         assertEquals(0, weeks);
 
-        durationInfo.setStartDateTime(dtCompare1);
-        durationInfo.setEndDateTime(dtCompare2);
+        durationInfo.setDateTimes(dtCompare1, dtCompare2);
         weeks = durationInfo.getCompleteWeeksBetweenDateTimes();
         // equivalent UTC times are 2017-12-31 15:00 and 2018-01-07 19:00, so should return 1
         assertEquals(1, weeks);

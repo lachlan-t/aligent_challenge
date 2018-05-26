451e3b335a743d11575c9a056f62a218cc280add
Lachlan Turner <lachlan.turner@gmail.com>
2018-05-26 20:20:17 +0930

    Implemented conversion to additional unit parameter.

--- src/AligentChallenge.java
+++ src/AligentChallenge.java
@@ -1,5 +1,8 @@
+import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.temporal.ChronoUnit;
+
+import static java.time.temporal.ChronoUnit.*;
 
 public class AligentChallenge {
 
@@ -10,7 +13,7 @@ public class AligentChallenge {
 
     public static void main(String[] args) {
         // check the number of input parameters - must be at least 2
-        if (args.length < 2) {
+        if (args.length < 2 || args.length > 3) {
             printUsage();
             return;
         }
@@ -27,16 +30,26 @@ public class AligentChallenge {
         // ensure date times are ordered
         OffsetDateTime[] dateTimes = DateTimeHelper.orderDateTimes(firstDateTime, secondDateTime);
 
+        // check for additional unit parameter
         ChronoUnit units = null;
+
+        if (args.length > 2) {
+            units = parseUnitsParameter(args[2]);
+            if (units == null) {
+                printUsage();
+                return;
+            }
+        }
+
         // perform and display results of calculations
         runCalculations(dateTimes[0], dateTimes[1], units);
     }
 
     private static void printUsage() {
-        System.out.println("Usage: java AligentChallenge DATETIME DATETIME [CONVERT_TO]");
+        System.out.println("Usage: java AligentChallenge DATETIME DATETIME [CONVERT_TO_UNITS]");
         System.out.println(" - DATETIME : Date time in following format: YYYY-MM-DDTHH:mm:SS[OFFSET]");
         System.out.println(" - OFFSET : Timezone offset in format: +HH:mm:SS");
-        System.out.println(" - CONVERT_TO : Additional time unit to calculate, S/M/H/Y -> Seconds/Minutes/Hours/Years");
+        System.out.println(" - CONVERT_TO_UNITS : Additional time unit to calculate, S/M/H/Y -> Seconds/Minutes/Hours/Years");
         System.out.println();
         System.out.println("e.g. java AligentChallenge 2018-01-01T12:00:00 2018-01-01T19:00:00+04:00:00 H");
     }
@@ -50,17 +63,58 @@ public class AligentChallenge {
 
         // get number of days between dates
         long days = durationInfo.getDaysBetweenDateTimes();
-        System.out.println(days + " Days");
+        System.out.print(days + " Complete Days");
+        if (units != null) {
+            long value = getConvertedValue(days, DAYS, units);
+            System.out.print("(" + value + " " + units + ")");
+        }
+        System.out.println();
 
+        // weekdays
         long weekdays = durationInfo.getWeekdaysBetweenDateTimes();
-        System.out.println(weekdays + " Weekdays");
+        System.out.print(weekdays + " Complete Weekdays");
+        if (units != null) {
+            long value = getConvertedValue(weekdays, DAYS, units);
+            System.out.print("(" + value + " " + units + ")");
+        }
+        System.out.println();
 
+        // complete weeks
         long weeks = durationInfo.getCompleteWeeksBetweenDateTimes();
-        System.out.println(weeks + " Complete weeks");
+        System.out.print(weeks + " Complete weeks");
+        if (units != null) {
+            long value = getConvertedValue(weeks, WEEKS, units);
+            System.out.print("(" + value + " " + units + ")");
+        }
+        System.out.println();
 
+        // additional units if specified
         if (units != null) {
             long unitsValue = durationInfo.getUnitsBetweenDateTimes(units);
             System.out.println(unitsValue + " " + units);
+        }
+    }
+
+    private static long getConvertedValue(long value, ChronoUnit baseUnit, ChronoUnit convertedUnit) {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime future = now.plus(value, baseUnit);
+        return convertedUnit.between(now, future);
+    }
+
+    // determine correct ChronoUnit object to use for conversions
+    private static ChronoUnit parseUnitsParameter(String parameter) {
+        switch (parameter) {
+            case SECONDS_PARAM:
+                return SECONDS;
+            case MINUTES_PARAM:
+                return MINUTES;
+            case HOURS_PARAM:
+                return HOURS;
+            case YEARS_PARAM:
+                return YEARS;
+            default:
+                System.err.println("Error parsing units parameter: " + parameter);
+                return null;
         }
     }
 }
